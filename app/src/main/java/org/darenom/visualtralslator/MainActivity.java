47d074bae1709254e46a91affe5f7af5b18cfed4
dma <areno.dm@gmail.com>
2017-04-26 16:30:45 +0200

    simpleOCR

--- app/src/main/java/org/darenom/visualtralslator/MainActivity.java
+++ app/src/main/java/org/darenom/visualtralslator/MainActivity.java
@@ -1,21 +1,32 @@
 package org.darenom.visualtralslator;
 
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.graphics.Bitmap;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.ButtonBarLayout;
+import android.util.Log;
+import android.util.SparseArray;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.vision.Frame;
+import com.google.android.gms.vision.text.TextBlock;
+import com.google.android.gms.vision.text.TextRecognizer;
 
 public class MainActivity extends AppCompatActivity {
 
+    private final String TAG = getClass().getSimpleName();
     static final int REQUEST_IMAGE_CAPTURE = 1;
 
     private ImageView mImageView;
     private Button mButton;
+    private TextView mText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +35,7 @@ public class MainActivity extends AppCompatActivity {
 
         mImageView = (ImageView) findViewById(R.id.bmp);
         mButton = (Button) findViewById(R.id.pic);
+        mText = (TextView) findViewById(R.id.text);
         mButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -46,7 +58,46 @@ public class MainActivity extends AppCompatActivity {
             Bundle extras = data.getExtras();
             Bitmap imageBitmap = (Bitmap) extras.get("data");
             mImageView.setImageBitmap(imageBitmap);
+            mText.setText(decode(imageBitmap));
         }
+    }
+
+    private String decode(Bitmap imageBitmap) {
+        String decode = "";
+        TextRecognizer textRecognizer = new TextRecognizer.Builder(this).build();
+        Frame frame = new Frame.Builder().setBitmap(imageBitmap).build();
+
+        SparseArray<TextBlock> texts = textRecognizer.detect(frame);
+        String[] langs = new String[texts.size()];
+        if (!textRecognizer.isOperational()) {
+            // Note: The first time that an app using a Vision API is installed on a
+            // device, GMS will download a native libraries to the device in order to do detection.
+            // Usually this completes before the app is run for the first time.  But if that
+            // download has not yet completed, then the above call will not detect any text,
+            // barcodes, or faces.
+            //
+            // isOperational() can be used to check if the required native libraries are currently
+            // available.  The detectors will automatically become operational once the library
+            // downloads complete on device.
+            Log.w(TAG, "Detector dependencies are not yet available.");
+
+            // Check for low storage.  If there is low storage, the native library will not be
+            // downloaded, so detection will not become operational.
+            IntentFilter lowstorageFilter = new IntentFilter(Intent.ACTION_DEVICE_STORAGE_LOW);
+            boolean hasLowStorage = registerReceiver(null, lowstorageFilter) != null;
+
+            if (hasLowStorage) {
+                Toast.makeText(this, R.string.low_storage_error, Toast.LENGTH_LONG).show();
+                Log.w(TAG, getString(R.string.low_storage_error));
+            }
+        }
+
+        for (int i = 0; i < texts.size(); i++){
+            TextBlock text = texts.valueAt(i);
+            decode = decode + text.getValue() + "\n";
+        }
+
+        return decode;
     }
 
 }
